701be654250e6b6964dee7117cb989511b1ccbcf
MollyMiao <peiyaoshi0321@gmail.com>
2022-08-31 17:00:04 -0400

    831

--- tracker/src/LineChart.js
+++ tracker/src/LineChart.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import Plot from 'react-plotly.js';
+import DataParser from "./DataParser";
 
 class LineChart extends React.Component {
     render() {
+        <DataParser />
       
         const trace = {
-            x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-            y: [1, 3, 6],
+            x: ['2021/01/27','2021/02/01','2021/02/05','2021/02/15','2021/02/22','2021/03/06',
+            '2021/03/21','2021/04/19','2021/05/03','2021/05/12','2021/05/21','2021/05/28',
+            '2021/05/30','2021/06/11','2021/06/17','2021/06/23','2021/06/29','2021/07/05',
+            '2021/07/08','2021/07/11','2021/07/24','2021/08/01','2021/08/10','2021/08/23',
+            '2021/09/10','2021/09/19','2022/05/21'],
+            y: [100,500,1000,2000,3000,4000,5000,6000,
+                7000,8000,9000,10000,11000,12000,13000,
+                14000,15000,16000,17000,18000,19000,20000,
+                21000,22000,23000,24000,29000],
             mode: "markers+lines",
             type: "scatter",
           };
@@ -16,7 +25,7 @@ class LineChart extends React.Component {
               <Plot
                 data={data}
                 layout={{
-                  title: "Fans Trend Graph",
+                  title: "小红书粉丝里程碑",
                 }}
               />
             </div>
@@ -26,3 +35,4 @@ class LineChart extends React.Component {
   }
 
 export default LineChart;
+
--- tracker/src/routes/fanstrend.js
+++ tracker/src/routes/fanstrend.js
@@ -1,8 +1,10 @@
 import LineChart from "../LineChart";
 
+
 export default function FansTrend() {
   return (
     <main style={{ padding: "1rem 0" }}>
+ 
       <LineChart />
     </main>
   );
