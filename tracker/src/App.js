8e221f83b8e8ca0f030fbcbe057f22e7243e78cd
MollyMiao <peiyaoshi0321@gmail.com>
2022-09-05 16:16:47 -0400

    Add faker and card details

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,4 +1,7 @@
 import { Outlet, Link } from "react-router-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 export default function App() {
   return (
@@ -15,6 +18,46 @@ export default function App() {
         <Link to="/fanstrend">FansTrend</Link>
       </nav>
       <Outlet />
+
+      <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          content="I like the subject"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          content="I like the writing"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+    </div>  
+
     </div>
+
+
+
   );
 }
