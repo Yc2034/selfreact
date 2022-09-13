e8cc7171d3a9957614a03dc7dac62913ce771bae
MollyMiao <peiyaoshi0321@gmail.com>
2022-09-13 15:35:44 -0400

    accordion

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'pig',
+    },
+    {
+        title: 'Why use React?',
+        content: 'haha',
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use by create component',
+    },
+];
 
 
 export default () => {
-    return <h1>test app</h1>;
+    return (
+        <div>
+            <Accordion items = {items} />
+        </div>
+    );
 };
