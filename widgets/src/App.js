a084e7824902708598e670bf0623de948faf3db0
MollyMiao <peiyaoshi0321@gmail.com>
2022-09-13 22:26:32 -0400

    search

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import Accordion from './components/Accordion';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
-    {
-        title: 'What is React?',
-        content: 'pig',
-    },
-    {
-        title: 'Why use React?',
-        content: 'haha',
-    },
-    {
-        title: 'How do you use React?',
-        content: 'You use by create component',
-    },
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
 ];
 
-
-export default () => {
-    return (
-        <div>
-            <Accordion items = {items} />
-        </div>
-    );
+const App = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 };
+export default App;
